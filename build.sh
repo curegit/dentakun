c82b2867655a01ab99fd38fe275c235dfd45d988
curegit <37978051+curegit@users.noreply.github.com>
2019-02-13 20:50:46 +0900

    シェルスクリプト中にカレントディレクトリを変更させる

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+cd `dirname $0`
 javac -verbose -sourcepath src -d bin src/dentakun/Calculator.java
 jar --verbose --create --file dentakun.jar --manifest manifest -C bin/ .
 chmod +x dentakun.jar
--- build_and_run.sh
+++ build_and_run.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
+cd `dirname $0`
 ./build.sh
 java -jar dentakun.jar
