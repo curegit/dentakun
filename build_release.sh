a01a662cd2e578e6d563b82a9649140a015a124a
curegit <37978051+curegit@users.noreply.github.com>
2019-04-28 13:28:37 +0900

    リリースのzipをつくるスクリプト

---
+++ build_release.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+cd "$(dirname "$0")" || exit
+if [ -e dentakun.zip ]; then
+  rm dentakun.zip
+fi
+./build.sh || exit
+zip -r dentakun.zip img dentakun.jar
--- clean.sh
+++ clean.sh
@@ -3,3 +3,4 @@ cd "$(dirname "$0")" || exit
 rm -r -f bin
 rm -r -f obj
 rm -f ./*.jar
+rm -f ./*.zip
