86209c8e70a84f8654cf03858dc17d97258a70af
curegit <37978051+curegit@users.noreply.github.com>
2019-04-28 13:27:25 +0900

    失敗したときにexitする

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 cd "$(dirname "$0")" || exit
-javac -verbose -sourcepath src -d bin src/dentakun/Calculator.java
-jar --verbose --create --file dentakun.jar --manifest manifest -C bin/ .
+javac -verbose -sourcepath src -d bin src/dentakun/Calculator.java || exit
+jar --verbose --create --file dentakun.jar --manifest manifest -C bin/ . || exit
 chmod +x dentakun.jar
--- build_and_run.sh
+++ build_and_run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 cd "$(dirname "$0")" || exit
-./build.sh
+./build.sh || exit
 java -jar dentakun.jar
